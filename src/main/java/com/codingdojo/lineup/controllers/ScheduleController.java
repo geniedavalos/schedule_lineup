eb76406b861cd6c06fb6dd36cb228915142176eb
geniedavalos <46758410+geniedavalos@users.noreply.github.com>
2019-08-26 17:16:13 -0700

    Update the jsp file location.

--- src/main/java/com/codingdojo/lineup/controllers/ScheduleController.java
+++ src/main/java/com/codingdojo/lineup/controllers/ScheduleController.java
@@ -34,7 +34,7 @@ public class ScheduleController {
 		model.addAttribute("schedule", s);
 		List<Schedule> schedules = scheServ.getSchedules();
 		model.addAttribute("schedules", schedules);
-		return "/calendar/fullCalendar.jsp";
+		return "/calendar/calendar.jsp";
 	}
 	
 	@RequestMapping("/addSchedule")
